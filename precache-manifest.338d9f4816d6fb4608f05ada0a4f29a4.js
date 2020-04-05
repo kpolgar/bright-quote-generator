8facd7af23a5323ba4ba2cd434eec2c3d3e0ee3e
Kinga Polgar <kpolgar27@gmail.com>
2020-04-05 07:32:12 -0400

    Updates

---
+++ precache-manifest.338d9f4816d6fb4608f05ada0a4f29a4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cf09ea701ac401623ecdcfe0ac99f092",
+    "url": "/bright-quote-generator/index.html"
+  },
+  {
+    "revision": "64076622aa93f43d01c5",
+    "url": "/bright-quote-generator/static/css/main.13b401ed.chunk.css"
+  },
+  {
+    "revision": "55975dfc17c2106adced",
+    "url": "/bright-quote-generator/static/js/2.5bc5c3d5.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/bright-quote-generator/static/js/2.5bc5c3d5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "64076622aa93f43d01c5",
+    "url": "/bright-quote-generator/static/js/main.f16735bd.chunk.js"
+  },
+  {
+    "revision": "67f06defa9e40be48440",
+    "url": "/bright-quote-generator/static/js/runtime-main.c3bd1890.js"
+  }
+]);
